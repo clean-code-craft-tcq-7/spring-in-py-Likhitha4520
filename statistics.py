23b1d6301830fb9073725cd8e486d2ec86396459
Likhitha4520 <likhitham.452000@gmail.com>
2025-07-29 16:25:14 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -2,12 +2,12 @@
 def calculateStats(numbers):
   # implement the computation of statistics here and return the results
   stats = {}
-if numbers:
+ if numbers:
   stats["avg"]=sum(numbers) /len(numbers)
   stats["max"] = max(numbers)
   stats["min"] = min(numbers)
-else:
+ else:
   stats["avg"]= float('nan')
   stats["max"] = float('nan')
   stats["min"] = float('nan')
-  return stats
+ return stats
